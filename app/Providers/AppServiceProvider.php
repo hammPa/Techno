7d3ec8e3ea4272af144ce883b3ab1e616cbc9965
hammPa <m.ilhamcoc70@gmail.com>
2026-09-19 00:12:23 +0700

    add https support in prod

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // untuk di production agar https, terutama di railway
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // 2. Paksa HTTPS jika bukan di lokal
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
